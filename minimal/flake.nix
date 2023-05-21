a701de16a2547a39c17e6833fe42b3ea4800e3d4
Zarak <7254237+zarak@users.noreply.github.com>
2023-05-21 09:21:40 +0500

    devShell is deprecated

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -1,17 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShell = with pkgs; mkShell {
+        devShells.default = with pkgs; mkShell {
           buildInputs = [ cowsay ];
         };
-      });
+      }
+    );
 }
